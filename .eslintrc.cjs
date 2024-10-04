86971c58ede21017e245bcd5b7ab2626ed29b2cd
Kun0319 <lukunyi9130@gmail.com>
2024-10-05 01:28:14 +0800

    chore: update ESLint configuration、remove unnecessary yml file

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,30 +1,27 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    node: true,
+    es2021: true,
   },
   extends: [
-    'standard',
-    'plugin:prettier/recommended',
-    'plugin:vue/vue3-essential'
-  ],
-  overrides: [
-    {
-      env: {
-        node: true
-      },
-      files: ['.eslintrc.{js,cjs}'],
-      parserOptions: {
-        sourceType: 'script'
-      }
-    }
+    "plugin:vue/vue3-recommended", // 推荐配置，捕获更多问题
+    "plugin:prettier/recommended", // Prettier 用于格式化
   ],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
+    ecmaVersion: "latest",
+    sourceType: "module", // 统一使用模块语法
   },
-  plugins: ['vue'],
+  plugins: ["vue"],
   rules: {
-    'vue/multi-word-component-names': 'off'
-  }
-}
+    "vue/multi-word-component-names": "off", // 关闭多单词组件名限制
+  },
+  globals: {
+    ref: "readonly",
+    reactive: "readonly",
+    computed: "readonly",
+    defineComponent: "readonly",
+    useRoute: "readonly", // 例子，添加 Nuxt 3 全局方法
+    useRouter: "readonly",
+  },
+};
